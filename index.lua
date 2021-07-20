7a22e848cf24209eaa878b137a0d2dc18df58af4
Albin Groen <albin.groen@gmail.com>
2021-07-20 16:23:55 +0200

    :tada: Initial commit

---
+++ index.lua
@@ -0,0 +1,5 @@
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+function example_func(self, a, b)
+    print("A is: ", a)
+    print("B is: ", b)
+end
---
+++ index.ts
@@ -0,0 +1,4 @@
+function example_func(a: any, b: any) {
+  console.log("A is: ", a);
+  console.log("B is: ", b);
+}
